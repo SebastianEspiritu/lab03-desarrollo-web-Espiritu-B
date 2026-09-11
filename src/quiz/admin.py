3e73303b2d90000106bc8f0149c3748ef78baf08
Sebastian Espiritu <sebastian.espiritu@tecsup.edu.pe>
2026-09-10 19:38:38 -0500

    feat: configure django admin, inlineformsets and test web validation

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Exam, Question, Choice
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title',)
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'exam', 'score')
+    list_filter = ('exam',)
+    search_fields = ('text',)
+    inlines = [ChoiceInline]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct')
+    list_filter = ('is_correct',)
